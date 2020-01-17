3e6974da41f25ac2c6b64e9df27c93f19138d24e
rabeeasehar <sehar198@gmail.com>
2020-01-17 17:50:12 +0100

    create context.java

---
+++ Context.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Demo {
+public static void main(String[] args) {
+ArrayList<String> aList = new ArrayList<String>();
+aList.add("A");
+aList.add("B");
+aList.add("C");
+aList.add("D");
+aList.add("E");
+    System.out.println("The ArrayList elements are: ");
+      for (String s: aList) {
+         System.out.println(s);
+      }
+Iterator it = aList.iterator();
+String str = "";
+while (it.hasNext()) {
+str = (String) it.next();
+if (str.equals("D")) {
+it.remove();
+break;}}
+System.out.println("\nThe ArrayList elements are: ");
+for (String s: aList) {
+System.out.println(s); 
+}}}
