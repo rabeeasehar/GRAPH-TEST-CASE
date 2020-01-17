8e3c1582b6550c17a625d86d64b01070ab206891
rabeeasehar <sehar198@gmail.com>
2020-01-17 18:29:11 +0100

    Update layout.java

--- layout.java
+++ layout.java
@@ -1,19 +1,8 @@
-import java.io.*;
-import java.util.*;
-class Test {
-    public static void main(String[] args)  
-    {  
-        ArrayList<String> list = new ArrayList<String>();  
-        list.add("A");  
-        list.add("B");  
-        list.add("C");  
-        list.add("D");  
-        list.add("E"); 
-// Iterator to traverse the list 
-Iterator it = list.iterator();
-System.out.println("List elements : ");
-        while (it.hasNext())  
-            System.out.print(it.next() + " ");  
-        System.out.println();  
-    }  
-} 
+for (Iterator<? extends Reference> it = refs.iterator(); 
+	it.hasNext();) {
+	Reference ref = it.next();	           
+	String name = ReferenceUtil.getReferenceString(ref);	           
+	Matcher m = filterPattern.matcher(name);	           
+        if (!m.find()) {	          
+             it.remove();
+	}}
