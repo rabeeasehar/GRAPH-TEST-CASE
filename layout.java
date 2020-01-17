9fddcb737f5abb713879f537980503b1b302bc6c
rabeeasehar <sehar198@gmail.com>
2020-01-17 18:32:33 +0100

    Update layout.java

--- layout.java
+++ layout.java
@@ -1,8 +1,8 @@
-	for (Iterator<? extends Reference> it = refs.iterator(); 
-it.hasNext();) {
+for (Iterator<? extends Reference> it = refs.iterator(); 
+	it.hasNext();) {
 	Reference ref = it.next();	           
 	String name = ReferenceUtil.getReferenceString(ref);	           
 	Matcher m = filterPattern.matcher(name);	           
         if (!m.find()) {	          
-             
+        it.remove();      
 	}}
