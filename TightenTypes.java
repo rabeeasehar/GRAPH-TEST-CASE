b3f1f9acdfc112d5764b5d7187e4a811d456f081
rabeeasehar <sehar198@gmail.com>
2020-01-26 03:19:15 +0100

    Create TightenTypes.java

---
+++ TightenTypes.java
@@ -0,0 +1,11 @@
+// Look for a function type in the argument list.
+
+        Iterator<Node> paramIter = n.children().iterator();
+        while (paramIter.hasNext() && paramIter.hasNext()) {
+          Node arg = paramIter.next();
+ 
+          if (arg.getJSType() instanceof FunctionType) {
+            actions.addAll(getImplicitActionsFromArgument(
+                arg,
+                ((FunctionType) arg.getJSType()).getTypeOfThis(),
+                param.getJSType())); 
